f3e02f7bf50deee25e813ceebde5bcd896bef770
ggihan <140435642+GGihan@users.noreply.github.com>
2025-08-17 13:49:45 +0200

    Add playable round with score tracking

--- script.js
+++ script.js
@@ -9,11 +9,37 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice() {
     let choice = prompt("What do you choose?", "-- rock, paper or scissors --");
-    return choice;
+    return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return console.log("I'ts a draw, try again!");
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        humanScore = humanScore + 1;
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        humanScore = humanScore + 1;
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        humanScore = humanScore + 1;
+        return console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    } else {
+        computerScore = computerScore + 1;
+        return console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+console.log(humanSelection);
+
+playRound(humanSelection, computerSelection);
